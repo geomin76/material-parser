a1d5c4c855a5308d9714f50ad7700528c55b951f
geomin76 <geom@vt.edu>
2020-12-24 19:15:07 -0500

    Added text parser

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+# from pdfminer.high_level import extract_text
+import textract
+
+def parse_pdf(filename):
+    # text = extract_text('sample.PDF')
+    # print(repr(text))
+    # ls = text.split()
+    # ls = text.split('\n')
+    # print(ls)
+    print("ok")
+
+def parse_doc(filename):
+    text = textract.process('sample.DOCX')
+    ls = text.split(b'\n\n')
+    print(ls)
+
 if __name__ == "__main__":
     print("hello world")
+    # parse_pdf('')
+    parse_doc('')
